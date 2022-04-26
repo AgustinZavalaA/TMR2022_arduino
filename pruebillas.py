6ed98206bbcd066d724f3cd0d8583e6fcba7dc9d
Yisuslalala <1930318@upv.edu.mx>
2022-04-26 00:26:49 -0500

    Sacando el vector resultante

---
+++ pruebillas.py
@@ -0,0 +1,10 @@
+import math
+
+def sen(grados):
+    return math.sin(math.radians(grados))
+
+def cos(grados):
+    return math.cos(math.radians(grados))
+
+print(100*cos(70))
+
--- vectores_ult.py
+++ vectores_ult.py
@@ -1,3 +1,25 @@
+"""
+la entrada esperada es un vector de tamaño 7, con las medidas mas recientes de los ultrasonicos (son 7), cada uno con un rango de (0,51) cm.
+Cada vector ya trae un angulo dependiendo del indice, son los siguientes:
+0: 90°
+1: 225°
+2: 180°
+3: 135°
+4: 45°
+5: 0°
+6: 315°
+   3   0   4
+    \  |  /
+2 --       -- 5
+    /     \ 
+    1      6
+    
+la salida que se espera es una tupla que representen la magnitud y el angulo del vector resultante por ejemplo:
+(5, 90) # significa que el vector resultante es de magnitud 5 y con angulo de 90 grados
+"""
+
+import math
+
 def med(vec):
     #print(vec)
     cont = 0
@@ -12,10 +34,40 @@ def med(vec):
     print(cont)
     return (media / cont)
 
+def cos(grados):
+    return math.cos(math.radians(grados))
+
+def sen(grados):
+    return math.sin(math.radians(grados))
+
 # Prueba de valores de los ultrasónicos
 # Alch no sé como piensas pasarle los valores reales pa no le sé al pov xd
 # Pero creo recordar que tenías una declaración de cada uno de ellos por ahí, así que jala papi
 
 vec = [51, 5, 15, 51, 51, 51, 51]
+angulos = [90, 225, 180, 135, 45, 0, 315]
 
-print(med(vec))
+vec_prueba = [100, 80, 150]
+angulos_prueba = [70, 210, 310]
+
+#print(med(vec))
+
+# Vx es la agrupacion de todos los vectores 
+Vx = 0
+Vy = 0
+sum_Vx = 0
+sum_Vy = 0
+
+# for para sacar la suma del vector Vx y Vy
+for i in range(len(vec_prueba)):
+    Vx = vec_prueba[i] * cos(angulos_prueba[i])
+    Vy = vec_prueba[i] * sen(angulos_prueba[i])
+    print('La operacion hasta el momento acumula: ' + str(Vx))
+    sum_Vx = sum_Vx + Vx
+    sum_Vy = sum_Vy + Vy
+print(sum_Vx)
+print(sum_Vy)
+
+Vr = ((((sum_Vx)**2) + ((sum_Vy)**2)) ** (0.5))
+
+print(Vr)
