021e3d7e5359ef07db72c03f9e4f660907e26311
AgustinZavalaA <1930120@upv.edu.mx>
2022-04-26 18:45:59 -0500

    refactor de vectores_ult

---
+++ vectores_python/test_vectores.py
@@ -0,0 +1,1 @@
+import vectores_ult
--- vectores_python/vectores_ult.py
+++ vectores_python/vectores_ult.py
@@ -20,71 +20,56 @@ la salida que se espera es una tupla que representen la magnitud y el angulo del
 
 import math
 
-def med(vec):
-    #print(vec)
-    cont = 0
-    media = 0
-    # Ciclo que se puede omitir si directamente ponemos la cantidad de ultrasónicos
-    for i in vec:
-        cont = cont + 1
-        media = i + media
-        print(cont)
-        print(media)
-    
-    print(cont)
-    return (media / cont)
 
 def cos(grados):
     return math.cos(math.radians(grados))
 
+
 def sen(grados):
     return math.sin(math.radians(grados))
 
-#Arco function that return the degree of the input
+
+# Arco function that return the degree of the input
 def arco(grados):
     return math.degrees(math.atan(grados))
+
+
+def calculate_vector(distance_vector: list[float], angle_vector: list[int]) -> tuple[float, float, float, float]:
+    vx = 0
+    vy = 0
+    sum_vx = 0
+    sum_vy = 0
+
+    # for para sacar la suma del vector Vx y Vy
+    for i in range(len(distance_vector)):
+        vx = distance_vector[i] * cos(angle_vector[i])
+        vy = distance_vector[i] * sen(angle_vector[i])
+        sum_vx = sum_vx + vx
+        sum_vy = sum_vy + vy
+
+    result_vector = (sum_vx**2 + sum_vy**2) ** 0.5
+
+    div = sum_vy / sum_vx
+    angle_result_vector = arco(div)
+
+    if sum_vy < 0:
+        angle_result_vector += 180
+
+    return sum_vx, sum_vy, angle_result_vector, result_vector
+
 
 # Prueba de valores de los ultrasónicos
 # Alch no sé como piensas pasarle los valores reales pa no le sé al pov xd
 # Pero creo recordar que tenías una declaración de cada uno de ellos por ahí, así que jala papi
+def main():
+    vec = [0.0, 51, 52, 51, 51, 51, 51]
+    angulos = [90, 225, 180, 135, 45, 0, 315]
 
-vec = [0.0, 51, 52, 51, 51, 51, 51]
-angulos = [90, 225, 180, 135, 45, 0, 315]
+    sum_vx, sum_vy, angle_result_vector, result_vector = calculate_vector(vec, angulos)
 
-vec_ustin = vec
+    print(f"El vector resultante es de magnitud {result_vector} y con angulo de {angle_result_vector} grados")
+    print(f"Los componentes del vector son {sum_vx} en X, {sum_vy} en Y")
 
-vec_prueba = [100, 80, 150]
-angulos_prueba = [70, 210, 310]
 
-#print(med(vec))
-
-# Vx es la agrupacion de todos los vectores 
-Vx = 0
-Vy = 0
-sum_Vx = 0
-sum_Vy = 0
-
-# for para sacar la suma del vector Vx y Vy
-for i in range(len(vec_ustin)):
-    Vx = vec_ustin[i] * cos(angulos[i])
-    Vy = vec_ustin[i] * sen(angulos[i])
-    print('La operacion hasta el momento acumula: ' + str(Vx))
-    sum_Vx = sum_Vx + Vx
-    sum_Vy = sum_Vy + Vy
-print('Valor de la suma del vector de X: ' + str(sum_Vx))
-print('Valor de la suma del vector de Y: ' + str(sum_Vy))
-
-Vr = ((((sum_Vx)**2) + ((sum_Vy)**2)) ** (0.5))
-
-div = sum_Vy / sum_Vx
-A = arco(div) 
-
-# if A < 0:
-#     A = A + 180
-
-if sum_Vy < 0:
-    A += 180
-
-print('Valor del vector resultante: ' + str(Vr))
-print('Valor del angulo: ' + str(A))
-
+if __name__ == "__main__":
+    main()
